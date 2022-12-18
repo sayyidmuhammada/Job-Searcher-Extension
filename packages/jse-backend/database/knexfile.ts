ba8e03a05c7ed2aa16d8eb735e2968c2d5463589
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-12-18 09:26:05 +0700

    setup deployment for backend

--- packages/jse-backend/database/knexfile.ts
+++ packages/jse-backend/database/knexfile.ts
@@ -9,15 +9,15 @@ function knexConfig(pathValue: string) {
   // Warning: this might be error
   // I give this statement for yarn run migrate
   dotenv.config({ path: pathValue === undefined ? "../.env" : pathValue });
+  const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+  const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
   return {
     development: {
       client: "postgresql",
       connection: {
-        port: Number(process.env.DB_PORT),
-        database: process.env.DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        connectionString: URL,
+        ssl: true,
       },
       pool: {
         min: 2,
